a67d77db652da4dacd873a17cf63e8e5f11bb18f
IamXtova <christophe.onuorah@gmail.com>
2022-08-04 18:05:33 +0100

    adding files

---
+++ src/constants/data.js
@@ -0,0 +1,114 @@
+const categories = [
+  {
+    id: 1,
+    img: require("../../assets/images/user2.png"),
+    heading: "Starter Set",
+    paragraph: "0 of 2 credentials verified",
+    backgroundColor: "#a6ff4d",
+  },
+
+  {
+    id: 2,
+    img: require("../../assets/images/visa.png"),
+    heading: "Trust Set",
+    paragraph: "0 of 5 credentials verified",
+    backgroundColor: "#4881ea",
+  },
+  {
+    id: 3,
+    img: require("../../assets/images/piggy-bank.png"),
+    heading: "Social Set",
+    paragraph: "0 of 4 credentials verified",
+    backgroundColor: "#ffaf4d",
+  },
+
+  {
+    id: 3,
+    img: require("../../assets/images/piggy-bank.png"),
+    heading: "Finance Set",
+    paragraph: "0 of 4 credentials verified",
+    backgroundColor: "#ffaf4d",
+  },
+];
+
+const starter = [
+  {
+    id: 1,
+    img: require("../../assets/images/user.png"),
+    heading: "Phone",
+    price: "5 Strength",
+    backgroundColor: "#fff",
+  },
+  {
+    id: 2,
+    img: require("../../assets/images/email2.png"),
+    heading: "Email",
+    price: "5 Strength",
+    backgroundColor: "#fff",
+  },
+];
+const trust = [
+  {
+    id: 1,
+    img: require("../../assets/images/fingerprint.png"),
+    heading: "Identity",
+    price: "20 Strength",
+    backgroundColor: "#fff",
+  },
+  {
+    id: 2,
+    img: require("../../assets/images/globe1.png"),
+    heading: "Sanction/AML Screen",
+    price: "20 Strength",
+    backgroundColor: "#fff",
+  },
+  {
+    id: 3,
+    img: require("../../assets/images/passport1.png"),
+    heading: "ID Document",
+    price: "20 Strength",
+    backgroundColor: "#fff",
+  },
+];
+
+const social = [
+  {
+    id: 1,
+    img: require("../../assets/images/facebook.png"),
+    heading: "Facebook",
+    price: "5 Strength",
+    backgroundColor: "#fff",
+  },
+  {
+    id: 2,
+    img: require("../../assets/images/google.png"),
+    heading: "Google",
+    price: "5 Strength",
+    backgroundColor: "#fff",
+  },
+  {
+    id: 3,
+    img: require("../../assets/images/linkedin.png"),
+    heading: "LinkedIn",
+    price: "5 Strength",
+    backgroundColor: "#fff",
+  },
+  {
+    id: 3,
+    img: require("../../assets/images/twitter.png"),
+    heading: "Twitter",
+    price: "5 Strength",
+    backgroundColor: "#fff",
+  },
+];
+
+const finance = [
+  {
+    id: 1,
+    img: require("../../assets/images/piggy1.png"),
+    heading: "Financial Account",
+    price: "20 Strength",
+    backgroundColor: "#fff",
+  },
+];
+export { categories, starter, trust, social, finance };
---
+++ src/constants/values.js
@@ -0,0 +1,33 @@
+const values = {
+  horizontalPadding: 25,
+  verticalPadding: 20,
+  h1Style: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  h2Style: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#404040",
+  },
+  h3Style: {
+    fontSize: 12,
+    fontWeight: "bold",
+  },
+  pStyle: {
+    color: "grey",
+    fontSize: 12,
+  },
+  pWhiteStyle: {
+    color: "white",
+    fontSize: 12,
+  },
+  h4Style: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#404040",
+    textAlign: "right",
+  },
+};
+
+export default values;
